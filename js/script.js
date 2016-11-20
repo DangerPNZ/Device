f5100c64f67694104cb46c86dd44b2369819a96a
DangerPNZ <dangerpnz@mail.ru>
2016-11-20 10:44:12 +0400

    Модальные окна и анимация (предварительная)

---
+++ js/script.js
@@ -0,0 +1,52 @@
+var feedback_btn = document.querySelector(".feedback_mail");
+var feedback_modal = document.querySelector(".feedback");
+var feedback_form = feedback_modal.querySelector(".feedback_form");
+var input_name = document.querySelector("#client_name");
+var input_mail = document.querySelector("#email");
+var message = document.querySelector("textarea");
+
+feedback_btn.addEventListener("click", function(event){
+  event.preventDefault();
+  feedback_modal.classList.add("feedback_open");
+  input_name.focus();
+  var feedback_modal_close = feedback_modal.querySelector(".close_btn");
+  feedback_modal_close.addEventListener("click", function(event){
+    event.preventDefault();
+    feedback_modal.classList.remove("feedback_open");
+  });
+  window.addEventListener("keydown", function(event){
+      if (event.keyCode === 27) {
+          if (feedback_modal.classList.contains("feedback_open")) {
+              feedback_modal.classList.remove("feedback_open");
+          }
+      }
+  });
+  feedback_form.addEventListener("submit", function(event) {
+    if (!input_name.value || !input_mail.value || !message.value){
+      event.preventDefault();
+    }
+  });
+  if (!message.value){
+    input_name.classList.add("send_error");
+  };
+});
+
+var map_expand = document.querySelector(".map");
+var map_modal = document.querySelector(".detailed_map");
+
+map_expand.addEventListener("click", function(event){
+  event.preventDefault();
+  map_modal.classList.add("map_expand");
+  var map_close = map_modal.querySelector(".close_btn");
+  map_close.addEventListener("click", function(event){
+    event.preventDefault();
+    map_modal.classList.remove("map_expand");
+  });
+  window.addEventListener("keydown", function(event){
+      if (event.keyCode === 27) {
+          if (map_modal.classList.contains("map_expand")) {
+              map_modal.classList.remove("map_expand");
+          }
+      }
+  });
+});
